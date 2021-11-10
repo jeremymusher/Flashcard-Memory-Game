2a4ed03e8ce551e183ae9c2f807efecef7c05def
jmusher <jeremy.musher@colorado.edu>
2021-11-10 16:56:17 -0700

    main functionality complete, reshuffle button working, pairs found tallying up, 20 cards in total

--- script.js
+++ script.js
@@ -1,40 +1,92 @@
-const cards = document.getElementsByClassName("front");
-const cardsArr = [].slice.call(cards);
+const front = document.getElementsByClassName("front");
+const frontArr = [].slice.call(front);
 const images = document.getElementsByClassName("back");
 const imagesArr = [].slice.call(images);
 
-let firstTurn = true;
-let card1Class = ""
-let card2Class = ""
+const deck = document.querySelector(".deck");
 
-cardsArr.forEach((element) => {
-  element.addEventListener("click", flipCard);
-});
+const cards = deck.children;
+const newDiv = document.createDocumentFragment();
+
+const shuffleButton = document.querySelector("#reshuffle")
+const correct = document.querySelector("#numCorrect")
+
+let firstTurn = true;
+let card1Class = "";
+let card2Class = "";
+let frontofCard1 = null;
+let frontofCard2 = null;
+let cardOneImg = null;
+let cardTwoImg = null;
+
+let pairsFound = 0;
+
+
+function position() {
+  return Math.floor(Math.random() * cards.length);
+}
+
+function reshuffle() {
+  frontArr.forEach((element) => element.style.display = "block") 
+  imagesArr.forEach((element) => element.style.display = "none") 
+  while (cards.length > 0) {
+    newDiv.appendChild(cards[position()]);
+  }
+  deck.appendChild(newDiv);
+  pairsFound = 0;
+  correct.innerHTML=`${pairsFound}`
+
+
+}
+
+reshuffle();
+
+shuffleButton.addEventListener("click", reshuffle)
+
+function applyEvent() {
+  frontArr.forEach((element) => {
+    element.addEventListener("click", flipCard);
+  });
+}
+
+applyEvent();
 
 function flipCard(event) {
   event.target.style.display = "none";
-  imagesArr[cardsArr.indexOf(event.target)].style.display = "block";
+  imagesArr[frontArr.indexOf(event.target)].style.display = "block";
 
   if (firstTurn == true) {
-    card1Class = `${imagesArr[cardsArr.indexOf(event.target)].className}`
-    cardOneImg = imagesArr[cardsArr.indexOf(event.target)]
-    frontofCard1 = event.target
+    card1Class = `${imagesArr[frontArr.indexOf(event.target)].className}`;
+    cardOneImg = imagesArr[frontArr.indexOf(event.target)];
+    frontofCard1 = event.target;
     firstTurn = false;
-    return;
   } else if (firstTurn == false) {
-    card2Class = `${imagesArr[cardsArr.indexOf(event.target)].className}`
-    cardTwoImg = imagesArr[cardsArr.indexOf(event.target)]
-    frontofCard2 = event.target
+    card2Class = `${imagesArr[frontArr.indexOf(event.target)].className}`;
+    cardTwoImg = imagesArr[frontArr.indexOf(event.target)];
+    frontofCard2 = event.target;
     checkPair(card1Class, card2Class);
+    firstTurn = true;
   }
 }
 
-function checkPair (x, y) {
-        console.log(x)
-        console.log(y)
-        if (x == y) {
-          console.log("pair")
-      } else {
-          console.log("not a pair")
-      }
+function checkPair(x, y) {
+  frontArr.forEach((element) => {
+    element.removeEventListener("click", flipCard);
+  });
+
+  setTimeout(function () {
+    if (x == y) {
+      console.log("pair found");
+      pairsFound += 1;
+      correct.innerHTML=`${pairsFound}`
+      applyEvent();
+    } else {
+      console.log("not a pair");
+      frontofCard1.style.display = "block";
+      frontofCard2.style.display = "block";
+      cardOneImg.style.display = "none";
+      cardTwoImg.style.display = "none";
+      applyEvent();
+    }
+  }, 750);
 }
